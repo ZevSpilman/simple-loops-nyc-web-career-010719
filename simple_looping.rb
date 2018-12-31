bcfcb74e874541cf29848cc65697e0f94ee3e6e7
Zev Spilman <zevspilman@yahoo.com>
2018-12-31 21:46:57 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -11,8 +11,8 @@ def loop_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   loop do 
    puts phrase
-   number_of_times = number_of_times - 1
-   if number_of_times < 1 
+   number_of_times -=
+   if number_of_times <= 0 
    break
   
 end
